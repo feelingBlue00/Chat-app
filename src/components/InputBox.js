2d15e20ba6c29a9fe4c33c9fba7fa262c9abf631
feelingBlue00 <pta230500x090800@gmail.com>
2020-11-06 16:13:51 +0700

    Create repo for this chat app demo

---
+++ src/components/InputBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class InputBox extends Component {
+
+    state = {
+        message: null,
+        isTyping: false
+    }
+
+    handleInput = (e) => {
+                
+    }
+
+    render() {
+        <input type="text" placeholder="Type a message..." onChange={}/>
+    }
+
+}
---
+++ src/components/LoginForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const initialState = {
+    username: "",
+    password: "",
+    usernameError: "",
+    passwordError: "",
+}
+
+export default class LoginForm extends Component {
+
+    
+
+}
