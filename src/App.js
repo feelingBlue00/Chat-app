c92aaabb434b5fdbb5a18692a8801bf78c069528
feelingBlue00 <pta230500x090800@gmail.com>
2020-11-08 12:30:26 +0700

    Modified for Login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <LoginForm />
     </div>
   );
 }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,44 +1,90 @@
-import React from 'react';
+import React from "react";
+import "../css/LoginForm.css";
 
 const initialState = {
-    username: "",
-    password: "",
-    usernameError: "",
-    passwordError: "",
-}
+  username: "",
+  password: "",
+  usernameError: "",
+  passwordError: "",
+};
 
 export default class LoginForm extends React.Component {
-    state = initialState;
+  state = initialState;
 
-    handleChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value});
-    };
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-    validate = () => {
-        let nameError = "";
-        if (!this.state.username) {
-            nameError = "Name cannot be blank";
-        }
-
-        let passwordError = "";
-        if (!this.state.password) {
-            passwordError = "Password cannot be blank";
-        }
-
-        if (nameError || passwordError) {
-            this.setState({nameError, passwordError});
-            return false;
-        }
-
-        return true;
-    };
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        
+  validate = () => {
+    let nameError = "";
+    if (!this.state.username) {
+      nameError = "Name cannot be blank";
     }
 
-    render() {
-
+    let passwordError = "";
+    if (!this.state.password) {
+      passwordError = "Password cannot be blank";
     }
+
+    if (nameError || passwordError) {
+      this.setState({ nameError, passwordError });
+      return false;
+    }
+
+    return true;
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const isValid = this.validate();
+    if (isValid) {
+      this.setState(initialState);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div style={{ fontWeight: "bold", fontSize: 18, textAlign: "center" }}>
+          Sad chat room
+        </div>
+        <br />
+        <div className="login">
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <input
+                name="userName"
+                type="text"
+                placeholder="Username"
+                value={this.state.username}
+                onChange={(event) => this.handleChange(event)}
+              />
+              <div style={{ color: "red", fontSize: 11 }}>
+                {this.state.usernameError}
+              </div>
+            </div>
+            <br />
+            <div>
+              <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                value={this.state.password}
+                onChange={(event) => this.handleChange(event)}
+              />
+              <div style={{ color: "red", fontSize: 11 }}>
+                {this.state.passwordError}
+              </div>
+            </div>
+            <br />
+            <div>
+              <button type="submit" target="_blank">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
