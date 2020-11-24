32e75f77c72dbdb914dfc90a7c3335a186d6e14e
feelingBlue00 <pta230500x090800@gmail.com>
2020-11-24 18:37:03 +0700

    Delete some junk files and add a folder for server

---
+++ client/src/components/MainApp.js
@@ -0,0 +1,55 @@
+import React from "react";
+import InputBox from "./ChatRoom/InputBox";
+import Messages from "./Message/Messages";
+import "../css/MainApp.css";
+
+export default class MainApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: [],
+      date: new Date().toLocaleString(),
+    };
+
+    this.sendMessage = this.sendMessage.bind(this);
+  }
+
+  sendMessage(message) {
+    const messages = this.state.messages;
+    const messageObj = {
+      username: this.props.username,
+      message: message,
+    };
+    messageObj.fromThisUser = true;
+
+    messages.push(messageObj);
+    this.setState({ messages: messages });
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          className="room-name"
+          style={{
+            fontWeight: "bold",
+            fontSize: 16,
+            textAlign: "center",
+            height: "30px",
+          }}
+        >
+          {this.props.username}
+        </div>
+        <hr />
+        <div className="chat-container">
+          <Messages
+            messages={this.state.messages}
+            username={this.props.username}
+          />
+          <InputBox sendMessage={this.sendMessage} />
+        </div>
+      </div>
+    );
+  }
+}
