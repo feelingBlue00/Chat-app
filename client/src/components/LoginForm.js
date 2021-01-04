21404b15550a3795070c8af73b8621e0397d449f
feelingBlue00 <pta230500x090800@gmail.com>
2021-01-04 18:40:26 +0700

    Revamp the app

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,105 +1,29 @@
-import React from "react";
-import shortid from "shortid";
-import "../css/LoginForm.css";
-import ContactList from "./ChatRoom/ContactList";
-import MainApp from "./MainApp";
+import React from 'react';
 
 const initialState = {
-  username: "",
-  usernameError: "",
-};
-
-export default class LoginForm extends React.Component {
-  state = initialState;
-
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  validate = () => {
-    let nameError = "";
-    if (!this.state.username) {
-      nameError = "Name cannot be blank";
-    }
-
-    if (nameError) {
-      this.setState({ nameError });
-      return false;
-    }
-
-    return true;
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const isValid = this.validate();
-    if (isValid) {
-      this.setState({
-        initialState,
-        submitted: true,
-        id: shortid.generate(),
-      });
-    }
-  };
-
-  render() {
-    if (this.state.submitted) {
-      return (
-        <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
-          <ContactList
-            className="contact"
-            style={{ float: "left", width: "20%" }}
-          />
-          <MainApp
-            className="main-app"
-            username={this.state.username}
-            style={{
-              marginLeft: "20&",
-              borderLeft: "2px solid black !important",
-            }}
-          />
-        </div>
-      );
-    }
-
-    return (
-      <div>
-        <h1
-          style={{
-            fontWeight: "bold",
-            fontSize: 18,
-            textAlign: "center",
-            display: "block",
-          }}
-        >
-          Enter your user name
-        </h1>
-        <br />
-        <div className="login">
-          <form onSubmit={this.handleSubmit}>
-            <div>
-              <input
-                name="username"
-                type="text"
-                placeholder="Username"
-                autoComplete="off"
-                value={this.state.username}
-                onChange={(event) => this.handleChange(event)}
-                required
-              />
-              <div style={{ color: "red", fontSize: 11 }}>
-                {this.state.usernameError}
-              </div>
-            </div>
-            <br />
-            <div>
-              <button type="submit" target="_blank">
-                Login
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
-  }
+    username: "",
+    userNameError: ""
 }
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = initialState;
+
+        this.handleChange = this.handleChange.bind(this);
+        this.validate = this.validate.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value;
+        })
+    }
+
+    render() { 
+        return (  );
+    }
+}
+ 
+export default LoginForm;
